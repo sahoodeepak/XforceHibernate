803018d93e0c484a35d9c5309a528a5745213493
Deepak <Deepak@DRS>
2018-06-07 16:43:43 +0530

    second commit with added xml and UserOperation.java on 07/06/2018

--- src/main/java/com/dev/beans/Userinfo.java
+++ src/main/java/com/dev/beans/Userinfo.java
@@ -1,46 +1,65 @@
 package com.dev.beans;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_info")
 public class Userinfo {
-	
+
+	@Id
+	@Column(name = "user_id")
 	private int User_id;
+	@Column(name = "first_name")
 	private String FirstName;
+	@Column(name = "last_name")
 	private String LastName;
+	@Column(name = "email")
 	private String Email_id;
+	@Column(name = "password")
 	private String Password;
-	
+
 	public int getUser_id() {
 		return User_id;
 	}
+
 	public void setUser_id(int user_id) {
 		User_id = user_id;
 	}
+
 	public String getFirstName() {
 		return FirstName;
 	}
+
 	public void setFirstName(String firstName) {
-																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																
+
 		FirstName = firstName;
 	}
+
 	public String getLastName() {
 		return LastName;
 	}
+
 	public void setLastName(String lastName) {
 		LastName = lastName;
 	}
+
 	public String getEmail_id() {
 		return Email_id;
 	}
+
 	public void setEmail_id(String email_id) {
 		Email_id = email_id;
 	}
+
 	public String getPassword() {
 		return Password;
 	}
+
 	public void setPassword(String password) {
 		Password = password;
 	}
-	
-	
 
-	
 }
--- src/main/java/com/dev/test/UserOperations.java
+++ src/main/java/com/dev/test/UserOperations.java
@@ -1,9 +1,30 @@
 package com.dev.test;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.dev.beans.Userinfo;
+
 public class UserOperations {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Configuration config = new Configuration().configure();
+		SessionFactory factory = config.buildSessionFactory();
+		Session session = factory.openSession();
+
+		Userinfo user = new Userinfo();
+		user.setUser_id(15);
+		user.setFirstName("sipu");
+		user.setLastName("sahoo");
+		user.setEmail_id("sipu@gmail.com");
+		user.setPassword("14234");
+
+		session.getTransaction().begin();
+		session.save(user);
+		session.getTransaction().commit();
+		session.close();
+		factory.close();
 
 	}
 
